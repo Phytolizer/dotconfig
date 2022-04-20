e097910ac56f01473c70351c7922132fd562cd5b
Kyle Coffey <me@phytolizer.dev>
2022-04-20 10:00:15 -0500

    change curdisk script to always probe root

--- i3blocks/blocks/curdisk.c
+++ i3blocks/blocks/curdisk.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <sys/statvfs.h>
 
-int main(int argc, char** argv) {
+int main(void) {
     struct statvfs buf = {0};
-    statvfs(argv[1], &buf);
+    statvfs("/", &buf);
     unsigned long avail = buf.f_bsize * buf.f_bavail;
     unsigned long max = buf.f_bsize * buf.f_blocks;
     double pct = (double)(max - avail) / (double)max * 100.0;
-    printf("%s: %6.1fG (%2.0f%%)\n", argv[1], (max - avail) / 1024.0 / 1024.0 / 1024.0, pct);
+    printf("DSK %5.1fG (%2.0f%%)\n", (max - avail) / 1024.0 / 1024.0 / 1024.0, pct);
 }
